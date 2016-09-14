95bd8dbf1ede813089a4e00b9da835133c55ca10
Martti Louhivuori <martti.louhivuori@csc.fi>
2016-09-14 18:09:13 +0300

    Split machine specifications into separate files
    Move machine specific definitions (hardware topology, MPI environment
    etc.) into separate files and generalise machine file generation.

--- generate-machfile.sh
+++ generate-machfile.sh
@@ -10,6 +10,8 @@ then
 	echo "    MIC_PPN          -- no. of MPI tasks per MIC card (default: 12)"
 	echo "    NODES (optional) -- number of nodes to use, if unset"
 	echo "                        SLURM_NNODES will be used"
+	echo "    MICS (optional)  -- no. of MICs to use, if unset will assume a"
+	echo "                        dual-socket layout"
 	exit
 fi
 
@@ -19,10 +21,13 @@ mach=tmp.machfile
 
 [[ ${MPIRUN:+x} ]] || MPIRUN=mpirun
 [[ ${NODES:+x} ]] || NODES=$SLURM_NNODES
+[[ ${MICS:+x} ]] || MICS=(0 1)
 [[ ${HOST_PPN:+x} ]] || HOST_PPN=1
 [[ ${MIC_PPN:+x} ]] || MIC_PPN=12
 
-echo "Generating a machinefile for $NODES dual-MIC nodes with $HOST_PPN and $MIC_PPN MPI tasks on each host CPU and MIC card, respectively."
+echo "Generating a machinefile for $NODES MIC nodes"
+echo "  using offload devices (${MICS[@]})"
+echo "  with $HOST_PPN and $MIC_PPN MPI tasks on each host CPU and MIC card, respectively."
 
 $MPIRUN -n $NODES -ppn 1 hostname > $host
 cat $host | sort | uniq > $mach
@@ -34,7 +39,7 @@ fi
 
 for i in $(cat $mach)
 do
-   for d in ${OFFLOAD_DEVICES//,/ }
+   for d in ${MICS[@]}
    do
       echo ${i}-mic${d}:${MIC_PPN} >> $OUTPUT
       echo ${i}:${HOST_PPN}     >> $OUTPUT
--- mic-job.sh
+++ mic-job.sh
@@ -9,60 +9,42 @@
 #SBATCH -p mic
 #SBATCH --gres=mic:1
 
-MIC_PPN=30    # no. of MPI tasks per MIC card
+# read machine specific settings (e.g. hardware topology)
+#   (see specs.taito for a SLURM example incl. support for half-node jobs
+#    and specs.salomon for a PBS example)
+source specs.taito
+
+# if needed, change the no. of MPI tasks per MIC card to a more optimal
+# value for your simulation system (see xx for a rough guideline)
+#MIC_PPN=30
+
+# uncomment the following lines to turn on debugging
+#export DEBUG=1
+#export I_MPI_DEBUG=5
 
 
-#============== MACHINE SPECIFIC (taito.csc.fi) ===============
-# Hardware definition (Taito)
-HOST_CORES=6  # no. of CPU cores in a processor
-MIC_CORES=61  # no. of MIC cores in a card (use 60 for compute)
-MIC_TPC=4     # no. of threads per MIC core
-# load any modules needed
-module load gromacs-env/5.1.1-mic
+#===== DO NOT CHANGE (skip below for the run commands) =================
 
-#affinity for dual mic jobs
-export KMP_AFFINITY=verbose,compact,1,0
-if [ $OFFLOAD_DEVICES == "0" ]
-then
-  export KMP_AFFINITY="explicit,proclist=[0,1,2,3,4,5],verbose"
-fi
-if [ $OFFLOAD_DEVICES == "1" ]
-then
-  export KMP_AFFINITY="explicit,proclist=[6,7,8,9,10,11],verbose"
-fi
-
-#=================== DO NO CHANGE ============================
-
-# set  no. of threads/tasks for the job on HOST side
-HOST_PPN=1    # no. of MPI tasks per CPU, only 1 is a sensible value
-HOST_PE=$(echo ${OFFLOAD_DEVICES//,/ }|wc -w  ) # no. of host CPUs per job, set to number of mic cards
-
+# calculate no. of threads for host/mic
 export HOST_PPN MIC_PPN
 export HOST_THREADS=$(( $HOST_CORES / $HOST_PPN ))
 export MIC_THREADS=$(( ($MIC_CORES - 1) * $MIC_TPC / $MIC_PPN ))
 
-
 # PME nodes for GROMACS (remove for others!)
-NPME=$(( $HOST_PE * $HOST_PPN * $SLURM_NNODES ))
+NPME=$(( $HOST_PE * $HOST_PPN * $NODES ))
 
-# Uncomment the following lines to turn on debugging
-#export DEBUG=1
-#export I_MPI_DEBUG=5
-
-# MPI environment
-export MPIRUN="mpiexec.hydra"
-export I_MPI_MIC=1
-export I_MPI_JOB_RESPECT_PROCESS_PLACEMENT=disable
-
-export MIC_KMP_AFFINITY=balanced
-export I_MPI_FABRICS=shm:dapl
-export I_MPI_DAPL_PROVIDER=ofa-v2-mlx4_0-1
-
+# default affinities (add verbose to see the actual placement)
+[[ ${KMP_AFFINITY:+x} ]] || KMP_AFFINITY=compact,1,0
+[[ ${MIC_KMP_AFFINITY:+x} ]] || MIC_KMP_AFFINITY=balanced
+export KMP_AFFINITY MIC_KMP_AFFINITY
 
 # generate a machine file for this run
-#   note: uses environmental variables MPIRUN, HOST_PPN, MIC_PPN, OFFLOAD_DEVICES, and 
+#   note: uses environmental variables MPIRUN, HOST_PPN, MIC_PPN, MICS, and
 #         NODES (optional)
 ./generate-machfile.sh machfile
+
+#===== END OF DO NOT CHANGE ============================================
+
 
 # define the commands to run on the HOST and MIC nodes with mic-launch.sh
 #   note: if using modules (above) CMD_PATH can most likely be empty
@@ -71,12 +53,9 @@ export CMD_HOST=mdrun_mpi
 export CMD_MIC=mdrun_mic
 export CMD_FLAGS="-npme ${NPME} -ntomp_pme ${HOST_THREADS} -dlb yes"
 
-
 # launch the job
-#   note: uses environmental variables MIC_THREADS, HOST_THREADS, CMD_PATH, 
-#         CMD_HOST, CMD_MIC, CMD_FLAGS, and MIC_KMP_AFFINITY and modifies 
+#   note: uses environmental variables MIC_THREADS, HOST_THREADS, CMD_PATH,
+#         CMD_HOST, CMD_MIC, CMD_FLAGS, and MIC_KMP_AFFINITY and modifies
 #         OMP_NUM_THREADS and LD_LIBRARY_PATH
-
-
 $MPIRUN -machinefile machfile ./mic-launch.sh
 
